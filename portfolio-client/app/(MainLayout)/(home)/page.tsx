49d0477e3eb48308231e1a1dd93c331d13e91213
MohammadJewelRana <js.rana0326@gmail.com>
2026-04-03 18:28:09 +0600

    about me section designed

--- portfolio-client/app/(MainLayout)/(home)/page.tsx
+++ portfolio-client/app/(MainLayout)/(home)/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import HeroSection from "./_components/HeroSection";
+import About from "./_components/About";
+ 
  
 const page = () => {
   return (
     <div>
       {/* <HeroSection /> */}
       <div className=" mx-auto max-w-7xl  px-4">
- lorem10000
+<About/>
+
+ 
       </div>
     </div>
   );
