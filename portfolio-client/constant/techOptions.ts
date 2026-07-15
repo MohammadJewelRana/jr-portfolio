ebc643e9b07a45536c4dfe48c80e0758bb8a19ff
MohammadJewelRana <js.rana0326@gmail.com>
2026-07-15 22:01:21 +0600

    changed techOptions

--- portfolio-client/constant/techOptions.ts
+++ portfolio-client/constant/techOptions.ts
@@ -1,10 +1,56 @@
 export const techOptions = [
+  // Frontend
   "HTML",
   "CSS",
+  "Sass",
+  "Tailwind CSS",
+  "Bootstrap",
   "JavaScript",
   "TypeScript",
   "React",
   "Next.js",
+  "Redux Toolkit",
+  "React Query",
+  "Framer Motion",
 
+  // Backend
+  "Node.js",
+  "Express.js",
+  "NestJS",
+
+  // Mobile
+  "React Native",
+  "Expo",
+
+  // Database
+  "MongoDB",
+  "Mongoose",
+  "PostgreSQL",
+  "MySQL",
+  "Firebase",
+
+  // Authentication
+  "JWT",
+  "OAuth",
+  "NextAuth.js",
+
+  // APIs
+  "REST API",
+ 
+ 
+
+  // Cloud & Deployment
+  "Vercel",
+  "Netlify",
+  "AWS",
+  "Cloudinary",
+
+  // Tools
+  "Git",
+  "GitHub",
+  "Docker",
+  "Postman",
+  "Figma",
+
+ 
 ];
-
