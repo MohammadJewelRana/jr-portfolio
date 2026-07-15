753aa595b2389c097d386d5755e28215dec59257
MohammadJewelRana <js.rana0326@gmail.com>
2026-07-15 23:01:18 +0600

    Update portfolio backend

--- portfolio-backend/src/app/module/project/project.service.ts
+++ portfolio-backend/src/app/module/project/project.service.ts
@@ -77,7 +77,7 @@ const getAllProjects = async (query: Record<string, unknown>) => {
 // 🔹 Get Single Project (by ID or slug)
 const getSingleProject = async (idOrSlug: string) => {
   if (!idOrSlug) throw new Error("Project identifier missing");
-  console.log(idOrSlug);
+  // console.log(idOrSlug);
 
   const result = await ProjectModel.findOne({
     slug: idOrSlug,
